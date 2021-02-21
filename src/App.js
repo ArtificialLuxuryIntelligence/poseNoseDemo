d95cc078066d89d4d9978ecd449a22633e1b8b02
Ali <adgk.dev@gmail.com>
2021-02-21 13:50:07 +0000

    tidy: separate demo component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,10 @@
-import React, { useState } from 'react';
-import './App.css';
-import withNosePose from './modules/nosePose/HOC/withNosePose.js';
-import Canvas from './Components/Canvas.js';
-import Controls from './Components/Controls';
+import React from 'react';
+import Demo from './Components/Demo';
 
-const STOPPING_RATIO_DEFAULT = 0.5;
-const SPEED_DEFAULT = 12;
-
-function App({ nosePose }) {
-  const {
-    unitCirclePositionRef,
-    unitSquarePositionRef,
-    configure,
-    configs,
-  } = nosePose;
-
-  // state for moving cursor
-  const [speed, setSpeed] = useState(SPEED_DEFAULT);
-  const [stoppingRatio, setStoppingRatio] = useState(STOPPING_RATIO_DEFAULT);
-
+export default function App() {
   return (
-    <div
-      className="App"
-      style={{
-        minHeight: '100vh',
-        display: 'flex',
-      }}
-    >
-      <Canvas
-        unitSquarePositionRef={unitSquarePositionRef}
-        unitCirclePositionRef={unitCirclePositionRef}
-        speed={speed}
-        stoppingRatio={stoppingRatio}
-      />
-      <Controls
-        // general controls
-        configure={configure}
-        configs={configs}
-        // moving cursor controls
-        speed={speed}
-        setSpeed={setSpeed}
-        stoppingRatio={stoppingRatio}
-        setStoppingRatio={setStoppingRatio}
-      />
+    <div>
+      <Demo />
     </div>
   );
 }
-
-export default withNosePose(
-  App,
-  { video: true, circleControl: true, squareControl: true }
-  // {
-  //   responsiveness: {
-  //     value: 0.1,
-  //   },
-  //   performance: {
-  //     fps: 10,
-  //   },
-  // }
-  // {
-  //   outer_bounding: {
-  //     x: [-50, 50],
-  //     y: [-35, 35],
-  //   },
-  // }
-);
-
-// export default withNosePose(App);
---
+++ src/Components/Demo.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import withNosePose from './../modules/nosePose/HOC/withNosePose.js';
+import Canvas from './Canvas.js';
+import Controls from './Controls';
+
+const STOPPING_RATIO_DEFAULT = 0.5;
+const SPEED_DEFAULT = 12;
+
+function Demo({ nosePose }) {
+  const {
+    unitCirclePositionRef,
+    unitSquarePositionRef,
+    configure,
+    configs,
+  } = nosePose;
+
+  // state for moving cursor
+  const [speed, setSpeed] = useState(SPEED_DEFAULT);
+  const [stoppingRatio, setStoppingRatio] = useState(STOPPING_RATIO_DEFAULT);
+
+  return (
+    <div
+      className="Demo"
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+      }}
+    >
+      <Canvas
+        unitSquarePositionRef={unitSquarePositionRef}
+        unitCirclePositionRef={unitCirclePositionRef}
+        speed={speed}
+        stoppingRatio={stoppingRatio}
+      />
+      <Controls
+        // general controls (from nosePose)
+        configure={configure}
+        configs={configs}
+        // moving cursor controls
+        speed={speed}
+        setSpeed={setSpeed}
+        stoppingRatio={stoppingRatio}
+        setStoppingRatio={setStoppingRatio}
+      />
+    </div>
+  );
+}
+
+export default withNosePose(
+  Demo,
+  { video: true, circleControl: true, squareControl: true }
+
+);
+
